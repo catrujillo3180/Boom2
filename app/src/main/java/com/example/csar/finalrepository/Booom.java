a6c16aa4523ff2eb484a57973380bffb5e802ee0
CristianCamilo <chambra1112@gmail.com>
2016-12-20 23:41:19 -0500

    1.1

--- app/src/main/java/com/example/csar/finalrepository/Booom.java
+++ app/src/main/java/com/example/csar/finalrepository/Booom.java
@@ -8,6 +8,7 @@ public class Booom extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_booom);
     }
 }
