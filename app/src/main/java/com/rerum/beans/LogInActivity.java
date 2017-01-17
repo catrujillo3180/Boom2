f65d45cd1fe4c00faa3a5141c6a2edfbb53b92bb
CristianCamilo <chambra1112@gmail.com>
2017-01-16 21:17:34 -0500

    1.1

--- app/src/main/java/com/rerum/beans/LogInActivity.java
+++ app/src/main/java/com/rerum/beans/LogInActivity.java
@@ -20,23 +20,36 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by santiago on 28/09/16.
  */
 
-public class LogInActivity extends BaseActivity implements View.OnClickListener {
+public class LogInActivity extends BaseActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "FacebookLogin";
+    private static final int RC_SIGN_IN = 9001;
 
     // FIREBASE
     private FirebaseAuth mFirebaseAuth;
@@ -51,6 +64,8 @@ public class LogInActivity extends BaseActivity implements View.OnClickListener 
     private Button laterButton;
     private EditText mailEdit;
     private EditText passEdit;
+
+    private GoogleApiClient mGoogleApiClient;
 
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -124,13 +139,26 @@ public class LogInActivity extends BaseActivity implements View.OnClickListener 
         // [END initialize_fblogin]
 
 
+        //Google login button
+        SignInButton mSignInButton = (SignInButton) findViewById(R.id.google_login_button);
+        mSignInButton.setColorScheme(SignInButton.COLOR_LIGHT);
+        mSignInButton.setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-
         mFirebaseAuth.addAuthStateListener(mAuthListener);
 
     }
@@ -146,7 +174,22 @@ public class LogInActivity extends BaseActivity implements View.OnClickListener 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed
+                Log.e(TAG, "Google Sign In failed.");
+            }
+        }
+
+        else{
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
@@ -182,7 +225,41 @@ public class LogInActivity extends BaseActivity implements View.OnClickListener 
                 Intent main = new Intent(this, MainActivity.class);
                 startActivity(main);
                 break;
+            case R.id.google_login_button:
+                signInGoogle();
+                break;
+            default:
+                return;
         }
+    }
+
+    private void signInGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            startMainActivity();
+                            finish();
+                        }
+                    }
+                });
     }
 
     private void startMainActivity()
@@ -192,6 +269,11 @@ public class LogInActivity extends BaseActivity implements View.OnClickListener 
 
     private void startMainActivityRestaurant()
     {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
